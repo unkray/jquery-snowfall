6764dbf3adf8ae2ecf695cb47d34e4c4b2f6094b
Andrew Sh <unknowname.x@gmail.com>
2018-12-14 14:04:33 +0600

    update 2018-12-14

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # jquery-snowfall
 snow falling effect implemented in jQuery
 
+Fork: https://github.com/kopipejst/jqSnow
+
 # Using
 
 ```
 $(document).ready( function(){
 	$.fn.snow({
-		flakeChar: [".","*","❅","❆","❄"], 
-		minSize: 20,
-		maxSize: 60,
-		newOn: 200,
-		flakeColor: ["#ffffff"],
-		durationMillis: null,
+		flakeChar: [".","*","❅","❆","❄"], // the HTML char to animate
+		minSize: 10, // min size of snowflake, 10 by default
+		maxSize: 20, // max size of snowflake, 20 by default
+		newOn: 200, // frequency in ms of appearing of new snowflake, 500 by default
+		flakeColor: ["#ffffff"], // array of colors , #FFFFFF by default
+		durationMillis: null, // stop effect after duration
 		target: ".target-block"
 	});
 });
--- jquery.snowfall.js
+++ jquery.snowfall.js
@@ -1,3 +1,23 @@
+/**
+ * jquery-snowfall - jQuery Snow Effect Plugin
+ *
+ * Available under GPL licence
+ *
+ * @version 1.0 (13. Dec 2018)
+ * @author Andrey Shabalin
+ * @requires jQuery
+ * @see https://github.com/unkray/jquery-snowfall
+ *
+ * @params flakeChar - the HTML char to animate
+ * @params minSize - min size of snowflake, 10 by default
+ * @params maxSize - max size of snowflake, 20 by default
+ * @params newOn - frequency in ms of appearing of new snowflake, 500 by default
+ * @params flakeColors - array of colors , #FFFFFF by default
+ * @params durationMillis - stop effect after duration
+ * @params target - target block (defaulf: document)
+ * @example $.fn.snow({ flakeChar: [".","*","❅","❆","❄"], minSize: 20, maxSize: 60, newOn: 200, flakeColor: ["#ffffff"], durationMillis: null, target: ".target-block" });
+ */
+
 (function($){
 	$.fn.snow=function(options){
 	var defaults = {
@@ -13,7 +33,7 @@
 		documentWidth = $(options.target).width(),
 		getRandomInt = function(min,max){ return Math.floor(Math.random() * (max - min + 1)) + min; };
 	$(options.target).prepend('<div id="snowfall" />');
-	var interval=setInterval(function(){
+	var interval = setInterval(function(){
 		var startPositionLeft = Math.random() * documentWidth - 100,
 				startOpacity = 0.9 + Math.random(),
 				sizeFlake = options.minSize + Math.random() * options.maxSize,
